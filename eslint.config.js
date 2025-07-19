9e8f603c1bfaee005c37083f339608db1f21f405
Jay Parmar <jaysparmar303@gmail.com>
2025-07-19 13:44:11 +0530

    refactor: update configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import antfu from '@antfu/eslint-config'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default antfu(
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    react: true,
+    stylistic: {
+      indent: 2,
+      semi: false,
+      quotes: 'single',
+      jsx: true,
+    },
+    ignores: ['src/openapi/**', '**/*.md'],
+  },
+  {
+    files: ['src/**/*{ts,tsx,js,jsx}'],
+    rules: {
+      'no-console': ['warn'],
+      'node/prefer-global/process': ['off'],
+      'react-hooks-extra/no-direct-set-state-in-use-effect': ['off'],
+      'react/no-unstable-default-props': ['off'],
+      'ts/consistent-type-definitions': ['error', 'type'],
+      // 'antfu/no-top-level-await': ['off'],
+      // 'node/prefer-global/process': ['off'],
+      // 'unicorn/filename-case': 'off',
+      // 'eslint-comments/no-unlimited-disable': 'off',
+      // 'node/no-process-env': ['warn'],
+      // 'no-unused-vars': ['warn'],
+      // 'unused-imports/no-unused-vars': ['warn'],
+      // 'perfectionist/sort-imports': ['warn'],
     },
   },
-])
+)
